0d9300eb0c8f00a10da933e2aed120f172b2b000
killcoder26 <1928081@kiit.ac.in>
2023-01-15 13:31:43 +0530

    Version 1.0

--- backend/index.js
+++ backend/index.js
@@ -17,153 +17,152 @@ mongoose.connect("mongodb://localhost:27017/dashboard", {
 
 const userSchema = new mongoose.Schema({
     id: Number,
-    gender:String,
-    race:String,
-    parental_edu:String,
-    lunch:String,
-    test_prep:String,
-    math:Number,
-    reading:Number,
-    writing:Number
+    gender: String,
+    race: String,
+    parental_edu: String,
+    lunch: String,
+    test_prep: String,
+    math: Number,
+    reading: Number,
+    writing: Number
 })
 const User = new mongoose.model("users", userSchema)
 
 
 //routes
-app.get("/",(req,res)=>{
-      User.find((err,data)=>{
-            if(!err)
-            {
-                console.log(data)
-                res.send(data);
-            }
-            else{console.log(err)}
-     })
+app.get("/", (req, res) => {
+    User.find((err, data) => {
+        if (!err) {
+            console.log(data)
+            res.send(data);
+        }
+        else { console.log(err) }
+    })
 
-     
-   
+
+
 })
-app.post("/add",(req,res)=>{
-    const {i, g, rc,p,l,t,m,r,w} = req.body
-    
+app.post("/add", (req, res) => {
+    const { i, g, rc, p, l, t, m, r, w } = req.body
+
     const user = new User({
-    id: i,
-    gender:g,
-    race:rc,
-    parental_edu:p,
-    lunch:l,
-    test_prep:t,
-    math:m,
-    reading:r,
-    writing:w
-        
+        id: i,
+        gender: g,
+        race: rc,
+        parental_edu: p,
+        lunch: l,
+        test_prep: t,
+        math: m,
+        reading: r,
+        writing: w
+
     })
     user.save(err => {
-        if(err) {
+        if (err) {
             res.send(err)
         } else {
-            res.send( { message: "Successfully submitted" })
+            res.send({ message: "Successfully submitted" })
         }
     })
 })
 
-app.post("/delete",(req,res)=>{
-    const {i} = req.body
-    
-    User.deleteOne({id: i}, (err, user) => {
-        if(user){
+app.post("/delete", (req, res) => {
+    const { i } = req.body
+
+    User.deleteOne({ id: i }, (err, user) => {
+        if (user) {
             res.send("deleted successfully")
         }
-        else{
+        else {
             res.send("error in del")
         }
 
-})
+    })
 })
 
-app.post("/edit",(req,res)=>{
-    const {idd,lu,tp} = req.body   
-   
-    User.findOneAndUpdate({"id":idd}, {"$set":{"lunch":lu, "test_prep":tp}}, {new: true}, (err, doc) => {
+app.post("/edit", (req, res) => {
+    const { idd, lu, tp } = req.body
+
+    User.findOneAndUpdate({ "id": idd }, { "$set": { "lunch": lu, "test_prep": tp } }, { new: true }, (err, doc) => {
         if (!err) {
             console.log("Successful updation");
             res.send(doc);
         }
-        else{
+        else {
             console.log(err);
             res.send("Error")
         }
-        
-    })});
 
-    app.post("/search",(req,res)=>{
-        const {sd} = req.body
-        
-        User.find({parental_edu: sd}, (err, user) => {
-            if(user){
-                res.send(user)
-            }
-            else{
-                res.send("error in search")
-            }
-    
     })
-    })
+});
 
-app.post("/searchid",(req,res)=>{
-        const {studid} = req.body
-        
-        User.find({id:studid}, (err, user) => {
-            if(user){
-                res.send(user)
-            }
-            else{
-                res.send("error in search")
-            }
-    
+app.post("/search", (req, res) => {
+    const { sd } = req.body
+
+    User.find({ parental_edu: sd }, (err, user) => {
+        if (user) {
+            res.send(user)
+        }
+        else {
+            res.send("error in search")
+        }
+
     })
+})
+
+app.post("/searchid", (req, res) => {
+    const { studid } = req.body
+
+    User.find({ id: studid }, (err, user) => {
+        if (user) {
+            res.send(user)
+        }
+        else {
+            res.send("error in search")
+        }
+
     })
+})
 
 
-   //db for post
+//db for post
 const userSchema2 = new mongoose.Schema({
-    name:String,
-    anoc:String,
-    time:String
+    name: String,
+    anoc: String,
+    time: String
 })
 
 const Userp = new mongoose.model("posts", userSchema2)
 
-    app.post("/post",(req,res)=>{
-        const {n,a,t} = req.body
-        
-        const user = new Userp({
-       
-        name:n,
-        anoc:a,
-        time:t
-            
-        })
-        user.save(err => {
-            if(err) {
-                res.send(err)
-            } else {
-                res.send( { message: "Successfully posted" })
-            }
-        })
-    })
-    
-    app.get("/find",(req,res)=>{
-        Userp.find((err,data)=>{
-              if(!err)
-              {
-                  console.log(data)
-                  res.send(data);
-              }
-              else{console.log(err)}
-       })
-    })
+app.post("/post", (req, res) => {
+    const { n, a, t } = req.body
 
-app.listen(5000,() => {
+    const user = new Userp({
+
+        name: n,
+        anoc: a,
+        time: t
+
+    })
+    user.save(err => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send({ message: "Successfully posted" })
+        }
+    })
+})
+
+app.get("/find", (req, res) => {
+    Userp.find((err, data) => {
+        if (!err) {
+            console.log(data)
+            res.send(data);
+        }
+        else { console.log(err) }
+    })
+})
+
+app.listen(5000, () => {
     console.log("BE started at port 5000")
 })
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {
     MDBFooter,
-    MDBContainer    
+    MDBContainer
 } from 'mdb-react-ui-kit';
 
 import { SocialIcon } from 'react-social-icons'
@@ -16,11 +16,11 @@ function Footer() {
                 <section>
                     <div className='iconbutton'>
 
-                        <SocialIcon url="https://facebook.com" target="_blank" network="facebook" style={{ height: 35, width: 35 }} />
-                        <SocialIcon url="https://instagram.com" target="_blank" network="instagram" style={{ height: 35, width: 35 }} />
-                        <SocialIcon url="mailto:sunandita.mody16@gmail.com" target="_blank" network="email" style={{ height: 35, width: 35 }} />
-                        <SocialIcon url="https://www.linkedin.com/in/sunandita-mody-93803520a/" target="_blank" style={{ height: 35, width: 35 }} />
-                        <SocialIcon url="https://github.com/Sunanditamody" network="github" target="_blank" style={{ height: 35, width: 35 }} />
+                        <SocialIcon url="https://facebook.com/anurag.bokaro" target="_blank" network="facebook" style={{ height: 35, width: 35 }} />
+                        <SocialIcon url="https://instagram.com/yup_its_me_anurag" target="_blank" network="instagram" style={{ height: 35, width: 35 }} />
+                        <SocialIcon url="mailto:anu.bokaro.ak@gmail.com" target="_blank" network="email" style={{ height: 35, width: 35 }} />
+                        <SocialIcon url="https://www.linkedin.com/in/killcoder26/" target="_blank" style={{ height: 35, width: 35 }} />
+                        <SocialIcon url="https://github.com/killcoder26" network="github" target="_blank" style={{ height: 35, width: 35 }} />
 
                     </div>
                 </section>
@@ -28,7 +28,7 @@ function Footer() {
 
             <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', textAlign: 'center' }}>
                 <Typography sx={{ color: '#11226b' }}>
-                    © 2022 Developed by: Sunandita Mody
+                    © 2022 Developed by: Anurag
                 </Typography>
             </div>
         </MDBFooter></div>
